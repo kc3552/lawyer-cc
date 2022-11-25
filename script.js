25ef483577958c82b47877a7155ec443886e141a
kerim calis <caliskerim35@gmail.com>
2022-11-25 15:26:47 +0300

    yeni eklemeler

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 
-alert("Sitemize Hoşgeldiniz! WhatsUp üzerinden yolladığınız ilk bilgi istemine;en kısa sürede cevap verilecek, ücret talep edilmeyecektir!");
+
+
+var left="[";
+var right="]";
+var msg="- AV.Çiğdem Çalış ÇAĞAN - Sitemize Hoş geldiniz! -";
+var speed=200;
+
+function scroll_title() {
+        document.title=left+msg+right;
+        msg=msg.substring(1,msg.length)+msg.charAt(0);
+        setTimeout("scroll_title()",speed);
+}
+scroll_title();
+ 
+var title = document.title;
+var alttitle = "Buraya gelir misin lütfen";
+window.onblur = function () { document.title = alttitle; };
+window.onfocus = function () { document.title = title; };
+
+
+4
+5
+ 
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+
